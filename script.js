2b8ba2cbe630a59b953eb3a9752e117a03d4d438
Renndy <40768140+Rennd9@users.noreply.github.com>
2023-05-19 19:52:29 +0700

    script.js
    Signed-off-by: Renndy <40768140+Rennd9@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-document.addEventListener("click", counterClick);
-  //varible
+const targetElement = document.getElementById("target-element");
+
+function counterClick(event) {
+  // Periksa apakah event target adalah elemen yang diinginkan
+  if (event.target === targetElement) {
+    // Tambahkan kode yang ingin dijalankan saat elemen tersebut diklik
+  }
+}
+
+document.addEventListener("click", counterClick);  //varible
     var audioList = [
         new Audio("audio/kuruto.mp3"),
         new Audio("audio/kuru1.mp3"),
@@ -63,66 +71,94 @@ document.addEventListener("click", counterClick);
     }
 
     function animateHerta() {
-        var counter_button = document.getElementById("counter-button");
-        var id = null;
+        const counter_button = document.getElementById("counter-button");
+        let id = null;
 
-        var random = Math.floor(Math.random() * 2) + 1;
-        var elem = document.createElement("img");
+        const random = Math.floor(Math.random() * 3) + 1;
+        const elem = document.createElement("img");
         elem.src = `img/hertaa${random}.gif`;
+        elem.classList.add("animate-herta-img");
         elem.style.position = "fixed";
-        elem.style.right = "15px";
+        elem.style.right = "-510px";
         elem.style.top = counter_button.getClientRects()[0].bottom + scrollY - 120 + "px"
         elem.style.zIndex = "-1";
         document.body.appendChild(elem);
 
-        var pos = -15;
-        var limit = window.innerWidth + -50;
+        let pos = -510;
+        const limit = window.innerWidth + 510;
         clearInterval(id);
-        id = setInterval(frame, 12);
-        function frame() {
+        id = setInterval(() => {
             if (pos >= limit) {
                 clearInterval(id);
                 elem.remove()
             } else {
-                pos += 15;
+                pos += 20;
                 elem.style.right = pos + 'px';
             }
-        }
+        }, 12);
     }
 
-    //end counter button
-// Variabel audio
-var audio = new Audio("audio/bgm.mp3"); // Ganti "nama_file_audio.mp3" dengan URL atau path file audio yang ingin digunakan
+    function animateImage(direction) {
+        const elem = document.createElement("img");
+        elem.src = "img/hertaa1.gif";
+        elem.classList.add("animate-image");
+        elem.style.position = "fixed";
+        elem.style.zIndex = "-1";
+      
+        let startPos, endPos, increment;
+        if (direction === "atas") {
+          startPos = window.innerHeight;
+          endPos = -elem.height;
+          increment = -10;
+          elem.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+          elem.style.top = startPos + "px";
+        } else if (direction === "bawah") {
+          startPos = -elem.height;
+          endPos = window.innerHeight;
+          increment = 10;
+          elem.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+          elem.style.top = startPos + "px";
+        } else if (direction === "kiri") {
+          startPos = window.innerWidth;
+          endPos = -elem.width;
+          increment = -10;
+          elem.style.left = startPos + "px";
+          elem.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+        } else if (direction === "kanan") {
+          startPos = -elem.width;
+          endPos = window.innerWidth;
+          increment = 10;
+          elem.style.left = startPos + "px";
+          elem.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+        }
+      
+        document.body.appendChild(elem);
+      
+        let pos = startPos;
+        const id = setInterval(() => {
+          if ((increment > 0 && pos >= endPos) || (increment < 0 && pos <= endPos)) {
+            clearInterval(id);
+            elem.remove();
+          } else {
+            pos += increment;
+            if (direction === "atas" || direction === "bawah") {
+              elem.style.top = pos + "px";
+            } else {
+              elem.style.left = pos + "px";
+            }
+          }
+        }, 12);
+      }
+      
+      // Contoh penggunaan
+      animateImage("atas"); // Gambar bergerak dari bawah ke atas
+      animateImage("bawah"); // Gambar bergerak dari atas ke bawah
+      animateImage("kiri"); // Gambar bergerak dari kanan ke kiri
+      animateImage("kanan"); // Gambar bergerak dari kiri ke kanan
+      // Variabel audio
+const slideElement = document.getElementById("slide-element");
+const audioElement = document.getElementById("audio-element");
 
-// Fungsi tombol pause
-function pauseAudio() {
-    audio.pause();
-}
-
-// Fungsi tombol reset
-function resetAudio() {
-    audio.currentTime = 0;
-}
-
-// Fungsi tombol autoloop
-function toggleAutoLoop() {
-    audio.loop = !audio.loop;
-}
-
-// Panggil fungsi toggleAutoLoop saat halaman dimuat
-window.onload = function() {
-    toggleAutoLoop();
-};
-// Fungsi autoplay
-function autoplayAudio() {
-    audio.play();
-}
-
-// Panggil fungsi autoplay saat halaman dimuat
-window.onload = function() {
-    autoplayAudio();
-};
-// Fungsi resume audio
-function resumeAudio() {
-    audio.play();
-}
+slideElement.addEventListener("touchstart", () => {
+  audioElement.pause();
+});
